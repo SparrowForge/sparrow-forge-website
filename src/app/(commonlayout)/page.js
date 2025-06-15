517f4768f75bc08fd147b1bf1c75db7751ce7813
sabrina-sprwforge <sabrina@sprwforge.com>
2025-06-15 19:59:38 +0600

    trusted startups section done

--- src/app/(commonlayout)/page.js
+++ src/app/(commonlayout)/page.js
@@ -2,11 +2,13 @@
 import IndustriesWeServe from "./component/homepage/IndustriesWeServe";
 import MainBanner from "./component/homepage/MainBanner";
 import TestimonialSection from "./component/homepage/TestimonialSection";
+import TrustedBy from "./component/homepage/TrustedBy";
 
 export default function Home() {
   return (
     <div className="w-[100%] mx-auto  min-h-screen">
       <MainBanner />
+      <TrustedBy/>
       <IndustriesWeServe/>
       <TestimonialSection />
 

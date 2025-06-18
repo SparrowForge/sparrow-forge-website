556ddef693a50547c37924ce0650121323fd3352
sabrina-sprwforge <sabrina@sprwforge.com>
2025-06-18 22:36:22 +0600

    our story section desigh fixed

--- src/app/(commonlayout)/component/design/OurStoryAvatarCard.jsx
+++ src/app/(commonlayout)/component/design/OurStoryAvatarCard.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import AvatarGroup from './AvatarGroup';
+import Image from 'next/image';
 
 const OurStoryAvatarCard = () => {
     return (
         <div className='bg-white rounded-[38px] flex flex-col justify-between items-start p-8'>
             <h1 className='text-[18px] font-bold text-green'>Meet the Minds Behind the Magic A Team of Thinkers, Makers, and Problem-Solver.</h1>
-            <AvatarGroup/>
-            
+
+            <Image
+                src="/story/Line1.svg"
+                width={30}
+                height={40}
+                alt='image'
+                className='hidden md:flex w-auto h-auto mr-4' />
+            <AvatarGroup />
         </div>
     );
 };

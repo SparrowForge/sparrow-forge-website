a6ff0f607ad21fac5c79bab8bf38fe962c73b208
sabrina-sprwforge <sabrina@sprwforge.com>
2025-06-19 20:46:51 +0600

    new blog card edit

--- src/app/(commonlayout)/component/design/NewBlogDesign.jsx
+++ src/app/(commonlayout)/component/design/NewBlogDesign.jsx
@@ -61,7 +61,7 @@ const NewBlogDesign = () => {
                             </Link>
                         </div>
 
-                        {/* Read More Button on hover */}
+                     
                         <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-500 z-20">
                             <Link href={item.link} target="_blank">
                                 <button className=" px-6 py-3 rounded hover:bg-white flex items-center gap-2 shadow-lg">

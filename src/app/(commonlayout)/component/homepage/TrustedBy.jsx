c5bbb9476fecc16f756e670ccdf85b9358a42a7e
sabrinara <sabrina@sprwforge.com>
2025-09-15 00:45:10 +0600

    client list updated

--- src/app/(commonlayout)/component/homepage/TrustedBy.jsx
+++ src/app/(commonlayout)/component/homepage/TrustedBy.jsx
@@ -7,9 +7,12 @@ import Link from "next/link";
 const data = [
   { id: 1, name: "BAC (British American College)", image: "/Client_List/bacbd-logo.png", href: "https://www.bacbd.org" },
   { id: 2, name: "Event World BD", image: "/Client_List/eventworldbd.png", href: "https://eventworldbd.com" },
-  { id: 3, name: "Holy Seed Agro", image: "/Client_List/holyseedagro.png", href: "#" },
+  { id: 3, name: "Holy Seed Agro", image: "/Client_List/holyseedagro.png", href: "https://www.holyseedagroltd.com/" },
   { id: 4, name: "Cryto Wallet", image: "", href: "https://vtroncard.com" },
   { id: 5, name: "CLAR", image: "/Client_List/CLAR.png", href: "https://clar.pt" },
+  { id: 6, name: "Cryto Bank", image: "/Client_List/cryptobanx.png", href: "https://www.holyseedagroltd.com/" },
+  { id: 7, name: "Blue Atlanticl", image: "/Client_List/blueatlanticl.png", href: "https://www.blueatlanticltd.com" },
+  { id: 8, name: "Solo Clash", image: "/Client_List/soloclash.png", href: "http://soloclash.com" },
 ];
 
 const TrustedBy = () => {
@@ -18,20 +21,25 @@ const TrustedBy = () => {
       <h1 className="text-center text-2xl md:text-[48px]  font-bold my-10 text-deepblue">Trusted by startups and global teams</h1>
       <Marquee pauseOnHover speed={50} gradient={false}>
         {data.map((item) => (
-          <div key={item.id} className="opacity-50">
+          <div key={item.id} className="opacity-50 hover:opacity-100" >
             <Link href={item?.href} target="_blank" className="mx-8 flex items-center justify-center gap-2 md:gap-3">
-              {item.image && (
+              {item.image ? (
                 <Image
                   src={item.image}
                   alt={item.name}
                   width={100}
                   height={100}
                   className="object-contain"
+                  title={item.name}
                 />
-              )}
+              ) : (
+                <p className="text-[20px] font-bold">{item.name}</p>
+              )
+
+              }
 
 
-              <p className="text-[20px] font-bold">{item.name}</p>
+
             </Link>
           </div>
         ))}
--- src/app/(commonlayout)/component/shared/SparrowLogo.jsx
+++ src/app/(commonlayout)/component/shared/SparrowLogo.jsx
@@ -7,7 +7,7 @@ const SparrowLogo = () => {
             <Image src="/newDesign/logo.png" alt="Logo" width={55} height={55} />
 
               <div className="leading-tight text-white">
-                <h1 className="text-xl font-bold tracking-tight">Sparrow Forge</h1>
+                <h1 className="text-xl font-bold tracking-tight line-clamp-5">Sparrow Forge</h1>
                 <p className="text-start text-[8px] font-medium tracking-[0.5em] uppercase mt-1">
                     FORGE THE FUTURE
                 </p>

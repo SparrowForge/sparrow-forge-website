093e0fb0daf52bc4dfb7b06c6ee9d741c2c6a01f
Md. Fayzur Rahman <mdfayzurrahman030@gmail.com>
2025-10-25 17:38:01 +0600

    all issues fix

--- src/app/[locale]/(commonlayout)/components/CaseStudies/CaseStudiesHeader.jsx
+++ src/app/[locale]/(commonlayout)/components/CaseStudies/CaseStudiesHeader.jsx
@@ -1,30 +1,30 @@
-"use client";
-import React from "react";
+'use client'
+import React from 'react'
 
-import { useTranslations } from "next-intl";
-import { LOCALE_KEYS } from "@/constants/localizationKeys";
-import Title from "../../shared/Title";
-import Button from "../../shared/Button";
+import { useTranslations } from 'next-intl'
+import { LOCALE_KEYS } from '@/constants/localizationKeys'
+import Title from '../../shared/Title'
+import Button from '../../shared/Button'
 
 const CaseStudiesHeader = () => {
-  const t = useTranslations();
+  const t = useTranslations()
 
   return (
-    <div className="flex flex-col md:flex-row justify-start md:justify-between items-start md:items-center gap-2 md:gap-4 p-2 md:p-0">
-      <div className="w-full md:w-2/4">
+    <div className='flex flex-col md:flex-row justify-start md:justify-between items-start md:items-center gap-2 md:gap-4 p-2 md:p-0'>
+      <div className='w-full md:w-2/4'>
         <Title
           title={t(LOCALE_KEYS.CASE_STUDIES_TITLE)}
           subtitle={t(LOCALE_KEYS.CASE_STUDIES_SUBTITLE)}
-          className="flex flex-col justify-center items-start gap-0 md:gap-4 md:my-16"
+          className='flex flex-col justify-center items-start gap-0 md:gap-4 md:my-16'
         />
       </div>
       <Button
-        image="/bannerImg/arrowRightUp.svg"
+        image='/bannerImg/arrowRightUp.svg'
         title={t(LOCALE_KEYS.CASE_STUDIES_BUTTON)}
-        className="bg-lightblue/10 text-lightblue text-sm md:text-[20px] px-4 md:px-6 py-2 md:py-3 rounded-full animation-zoom-out-hover"
+        className='bg-lightblue/10 text-lightblue text-sm md:text-[20px] px-4 md:px-6 py-2 md:py-3 rounded-full animation-zoom-out-hover'
       />
     </div>
-  );
-};
+  )
+}
 
-export default CaseStudiesHeader;
+export default CaseStudiesHeader
--- src/app/[locale]/(commonlayout)/components/CaseStudies/CaseStudiesStats.jsx
+++ src/app/[locale]/(commonlayout)/components/CaseStudies/CaseStudiesStats.jsx
@@ -1,29 +1,44 @@
-"use client";
-import React from "react";
-import { useTranslations } from "next-intl";
-import { LOCALE_KEYS } from "@/constants/localizationKeys";
+'use client'
+import React from 'react'
+import { useTranslations } from 'next-intl'
+import { LOCALE_KEYS } from '@/constants/localizationKeys'
 
 const CaseStudiesStats = () => {
-  const t = useTranslations();
+  const t = useTranslations()
 
   const stats = [
-    { value: "95%", label: t(LOCALE_KEYS.CASE_STATS_CLIENT_SATISFACTION) },
-    { value: "100%", label: t(LOCALE_KEYS.CASE_STATS_ROI) },
-    { value: "97%", label: t(LOCALE_KEYS.CASE_STATS_DELIVERY) },
-  ];
+    {
+      value: '95%',
+      label: t(LOCALE_KEYS.CASE_STATS_CLIENT_SATISFACTION),
+      description: t(LOCALE_KEYS.CASE_STATS_CLIENT_SATISFACTION_DESCRIPTION),
+    },
+    {
+      value: '100%',
+      label: t(LOCALE_KEYS.CASE_STATS_ROI),
+      description: t(LOCALE_KEYS.CASE_STATS_ROI_DESCRIPTION),
+    },
+    {
+      value: '97%',
+      label: t(LOCALE_KEYS.CASE_STATS_DELIVERY),
+      description: t(LOCALE_KEYS.CASE_STATS_DELIVERY_DESCRIPTION),
+    },
+  ]
 
   return (
-    <div className="grid md:grid-cols-3 divide-y md:divide-y-0 md:divide-x divide-gray-300 mt-8 bg-lightblue/10 p-6 rounded-lg text-start w-full">
+    <div className='grid md:grid-cols-3 divide-y md:divide-y-0 md:divide-x divide-gray-300 mt-8 bg-lightblue/10 p-6 rounded-lg text-start w-full'>
       {stats.map((stat, idx) => (
-        <div key={idx} className="pl-12 py-2">
-          <h3 className="text-2xl md:text-[30px] lg:text-[62px] font-semibold text-lightblue">
+        <div key={idx} className='pl-12 py-2'>
+          <h3 className='text-2xl md:text-[30px] lg:text-[62px] font-semibold text-lightblue'>
             {stat.value}
           </h3>
-          <p className="text-lightblue text-sm md:text-[18px]">/{stat.label}</p>
+          <p className='text-lightblue text-sm md:text-[18px] font-bold'>/{stat.label}</p>
+          <p className='text-lightblue text-sm md:text-[16px] my-2'>
+            {stat.description}
+          </p>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default CaseStudiesStats;
+export default CaseStudiesStats

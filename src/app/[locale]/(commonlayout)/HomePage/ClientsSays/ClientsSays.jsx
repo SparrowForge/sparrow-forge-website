aa696ea519125845f9c255e0e8fad0432c2dcef2
Md. Fayzur Rahman <mdfayzurrahman030@gmail.com>
2025-10-26 17:35:24 +0600

    updated

--- src/app/[locale]/(commonlayout)/HomePage/ClientsSays/ClientsSays.jsx
+++ src/app/[locale]/(commonlayout)/HomePage/ClientsSays/ClientsSays.jsx
@@ -1,15 +1,15 @@
-import ClientsSaysHeader from "../../components/ClientsSays/ClientsSaysHeader";
-import ClientsSaysSlider from "../../components/ClientsSays/ClientsSaysSlider";
+import ClientsSaysHeader from '../../components/ClientsSays/ClientsSaysHeader'
+import ClientsSaysSlider from '../../components/ClientsSays/ClientsSaysSlider'
 
 const ClientsSays = () => {
-    return (
-        <div className='bg-lightblue/10 w-full'>
-            <div className="lg:w-[1194px] mx-auto flex flex-col items-start py-10 px-4">
-                <ClientsSaysHeader />
-                <ClientsSaysSlider/>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className='bg-lightblue/10 w-full'>
+      <div className='lg:w-[1194px] mx-auto flex flex-col items-start py-10 px-4'>
+        <ClientsSaysHeader />
+        <ClientsSaysSlider />
+      </div>
+    </div>
+  )
+}
 
-export default ClientsSays;
+export default ClientsSays
--- src/app/[locale]/(commonlayout)/components/Banner/HomeBannerAbove.jsx
+++ src/app/[locale]/(commonlayout)/components/Banner/HomeBannerAbove.jsx
@@ -1,64 +1,69 @@
-"use client";
-import Image from "next/image";
-import ButtonTitle from "../../shared/ButtonTitle";
-import Button from "../../shared/Button";
-import { LOCALE_KEYS } from "@/constants/localizationKeys";
-import { useLocalization } from "@/hooks/useLocalization";
+'use client'
+import Image from 'next/image'
+import ButtonTitle from '../../shared/ButtonTitle'
+import Button from '../../shared/Button'
+import { LOCALE_KEYS } from '@/constants/localizationKeys'
+import { useLocalization } from '@/hooks/useLocalization'
 
 const HomeBannerAbove = () => {
-    const { t } = useLocalization();
+  const { t } = useLocalization()
 
-    return (
-        <div className="flex flex-col justify-start items-start py-6 sm:py-8 md:py-10 px-4 md:px-0 gap-3 md:gap-4 w-full md:max-w-[500px]">
-            {/* Achievements Banner */}
-            <div className="flex justify-center items-center gap-0 md:gap-2 py-1 px-1 md:px-3 bg-gray/5 text-white rounded-full mb-4 md:mb-0 text-[10px] md:text-[16px]">
-                <ButtonTitle title={t(LOCALE_KEYS.ACHIEVEMENTS)} />
-                <p>{t(LOCALE_KEYS.ACHIEVEMENTS_DESC)}</p>
-                <Image
-                    src="/bannerImg/arrowRightUpWhite.svg"
-                    alt="arrow"
-                    width={40}
-                    height={40}
-                    className="w-auto h-auto"
-                />
-            </div>
+  return (
+    <div className='flex flex-col justify-start items-start py-6 sm:py-8 md:py-10 px-4 md:px-0 gap-3 md:gap-4 w-full md:max-w-[500px]'>
+      {/* Achievements Banner */}
+      <div className='flex justify-center items-center gap-0 md:gap-2 py-1 px-1 md:px-3 bg-gray/5 text-white rounded-full mb-4 md:mb-0 text-[10px] md:text-[16px]'>
+        <ButtonTitle title={t(LOCALE_KEYS.ACHIEVEMENTS)} />
+        <p>{t(LOCALE_KEYS.ACHIEVEMENTS_DESC)}</p>
+        <Image
+          src='/bannerImg/arrowRightUpWhite.svg'
+          alt='arrow'
+          width={40}
+          height={40}
+          className='w-auto h-auto'
+        />
+      </div>
 
-            <div className="hidden md:flex flex-col">
-                {/* Main Heading */}
-               <h1 className="text-2xl sm:text-[40px] lg:text-[60px] text-white font-bold leading-snug text-start">
-                    {t(LOCALE_KEYS.BUILD_YOUR)} <br />
-                    <span className="text-yellow-500">{t(LOCALE_KEYS.DREAM_PRODUCT)}</span> <br />
-                    {t(LOCALE_KEYS.FASTER_DELIVERY)}
-                </h1>
+      <div className='hidden md:flex flex-col'>
+        {/* Main Heading */}
+        <h1 className='text-2xl sm:text-[40px] lg:text-[60px] text-white font-bold leading-snug text-start'>
+          {t(LOCALE_KEYS.BUILD_YOUR)} <br />
+          <span className='text-yellow-500'>
+            {t(LOCALE_KEYS.DREAM_PRODUCT)}
+          </span>{' '}
+          <br />
+          {t(LOCALE_KEYS.FASTER_DELIVERY)}
+        </h1>
 
-                {/* Description */}
-          <p className="text-sm sm:text-base lg:text-[20px] text-gray/40 font-medium text-start">
-                    {t(LOCALE_KEYS.BANNER_DESCRIPTION)}
-                </p>
-            </div>
-            {/* mobile view */}
-            <div className=" flex md:hidden flex-col mb-5 gap-2">
-                {/* Main Heading */}
-                <h1 className="text-2xl md:text-[20px] lg:text-[60px] text-white font-bold text-start">
-                    {t(LOCALE_KEYS.BUILD_YOUR)} {" "}
-                    <span className="text-yellow-500">{t(LOCALE_KEYS.DREAM_PRODUCT)}</span> {" "}
-                    {t(LOCALE_KEYS.FASTER_DELIVERY)}
-                </h1>
+        {/* Description */}
+        <p className='text-sm sm:text-base lg:text-[20px] text-gray/40 font-medium text-justify'>
+          {t(LOCALE_KEYS.BANNER_DESCRIPTION)}
+        </p>
+      </div>
+      {/* mobile view */}
+      <div className=' flex md:hidden flex-col mb-5 gap-2'>
+        {/* Main Heading */}
+        <h1 className='text-2xl md:text-[20px] lg:text-[60px] text-white font-bold text-start'>
+          {t(LOCALE_KEYS.BUILD_YOUR)}{' '}
+          <span className='text-yellow-500'>
+            {t(LOCALE_KEYS.DREAM_PRODUCT)}
+          </span>{' '}
+          {t(LOCALE_KEYS.FASTER_DELIVERY)}
+        </h1>
 
-                {/* Description */}
-                <p className="text-[12px] md:text-[20px] text-gray/40 font-[500px] text-start">
-                    {t(LOCALE_KEYS.BANNER_DESCRIPTION)}
-                </p>
-            </div>
+        {/* Description */}
+        <p className='text-[12px] md:text-[20px] text-gray/40 font-[500px] text-start'>
+          {t(LOCALE_KEYS.BANNER_DESCRIPTION)}
+        </p>
+      </div>
 
-            {/* CTA Button */}
-            <Button
-                image="/bannerImg/arrowRightUpWhite.svg"
-                title={t(LOCALE_KEYS.LETS_GET_STARTED)}
-                className="bg-lightblue text-white text-sm lg:text-[20px] px-4 lg:px-6 py-2 md:py-3 rounded-full "
-            />
-        </div>
-    );
-};
+      {/* CTA Button */}
+      <Button
+        image='/bannerImg/arrowRightUpWhite.svg'
+        title={t(LOCALE_KEYS.LETS_GET_STARTED)}
+        className='bg-lightblue text-white text-sm lg:text-[20px] px-4 lg:px-6 py-2 md:py-3 rounded-full '
+      />
+    </div>
+  )
+}
 
-export default HomeBannerAbove;
+export default HomeBannerAbove
